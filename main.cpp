26083fc28a36af8d5f0051d56946f7c01f45ce80
unknown <mkiricenko289@gmail.com>
2020-11-27 18:31:39 +0200

    add delet elements function

--- main.cpp
+++ main.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 
 int* fillArray(int size);
+int* deleteNumbers(int* arr, int size, int K, int N);
+void printArr(int* arr, int size);
 
 int main() {
 	int size;
 	cout << "Input array size: ";
 	cin >> size;
 	int* arr = fillArray(size);
-
+	printArr(arr, size);
+	int* newArr = deleteNumbers(arr, size, 5, 3);
+	printArr(newArr, size - 3);
 
 	delete arr;
 	return 0;
@@ -22,4 +26,29 @@ int* fillArray(int size)
 		arr[i] = rand() % 50 + 1;
 	}
 	return arr;
+}
+
+int* deleteNumbers(int* arr, int size, int K, int N)
+{
+	int newSize = size - K;
+	int* newArr = new int[newSize];
+	for (int i = 0, j = 0; i < size; i++) {
+		if (i >= N && i < N + K) {
+
+		}
+		else {
+			newArr[j] = arr[i];
+			j++;
+		}
+	}
+	return newArr;
+}
+
+void printArr(int* arr, int size)
+{
+	cout << "Array: " << endl;
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
 }
